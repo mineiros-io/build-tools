342d17b85a256023e188dffd1f7ddf7e0963caba
Marius Tolzmann <marius@mineiros.io>
2022-01-23 21:15:00 +0100

    fix: install terraform before switching user

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
+if [ -n "${TF_VERSION}" ]; then
+  tfswitch "${TF_VERSION}"
+fi
+
 if [ -n "${USER_UID}" ]; then
   adduser -u "${USER_UID}" --shell /bin/bash -D user
   install -m 700 -o user -g user -d /home/user/.ssh
   install -m 600 -o user -g user /root/.ssh/known_hosts /home/user/.ssh
 
   exec su user -c "${*@Q}"
-fi
-
-if [ -n "${TF_VERSION}" ]; then
-  tfswitch "${TF_VERSION}"
 fi
 
 exec "${@}"
